140c498219ce9156914b1ba9e777ae5cc9170eb3
Артём Самунджян <samujan@yandex.ru>
2024-11-19 22:45:43 +0400

    правки по фронту

---
+++ LiteInvestMainFront/Program.cs
@@ -0,0 +1,35 @@
+using LiteInvestMainFront.Components;
+using LiteInvestMainFront.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDevExpressBlazor(config => config.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5);
+builder.Services.AddBlazorBootstrap();
+
+builder.Services.AddMvc();
+
+builder.Services.AddSingleton<ApiDataService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
