f77301d5fc60b58e183b0dd0ed2d522cde4e98d6
FinAlgoTrader <algofinteh@gmail.com>
2024-11-26 15:35:20 +0400

    Пришлось вручную слияние делать

---
+++ LiteInvestMainFront/Components/Services/JsInteropService.cs.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+namespace LiteInvestMainFront.Services
+{
+	public class JsInteropService : IDisposable
+	{
+		public DotNetObjectReference<JsInteropService> ServiceObjectDotNetReference { get; set; } = null;
+		public event Action<string, bool, bool> OnKeyUp;
+		public event Action<string, bool, bool> OnKeyDown;
+		public JsInteropService()
+		{
+			ServiceObjectDotNetReference = DotNetObjectReference.Create(this);
+		}
+		[JSInvokable("KeyDown")]
+		public void OnKeyDownJS(string keyCode, bool isCtrl, bool isShift)
+		{
+			OnKeyDown?.Invoke(keyCode, isCtrl, isShift);
+		}
+		[JSInvokable("KeyUp")]
+		public void OnKeyUpJS(string keyCode, bool isCtrl, bool isShift)
+		{
+			OnKeyUp?.Invoke(keyCode, isCtrl, isShift);
+		}
+		public void Dispose()
+		{
+			ServiceObjectDotNetReference?.Dispose();
+		}
+	}
+}
